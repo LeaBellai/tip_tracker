406e26d26b1c38d23ce73e833d946c9425611b47
Lea D. Bellai <leabellai41@gmail.com>
2016-05-16 21:03:22 -0400

    added modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-
-   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :name 
-      ) }
-
-   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, 
-     :current_password, :name  ) }
-  def after_sign_in_path_for(resource)
-    if session[:user_return_to] == nil
-      tips_path
-    else
-      super
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
-
- end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :tips
-  resources :tips
-  resources :tips
-  resources :tips
+
   resources :tips
   devise_for :users
   get 'welcome/index'
@@ -11,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'tips#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
